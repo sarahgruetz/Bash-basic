173e4eb55b22f8999090fd377387f6c39bd3de61
sarahgruetz <sarah.gruetz@gmail.com>
2021-09-30 15:59:26 -0300

    Exercicios 01 Unix Shell

---
+++ log.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+echo "O diretório atual é: $PWD"
+
+nome='Sarah Beatriz Gruetzmacher'
+idade=26
+
+echo "Nome completo: $nome"
+
+echo "Idade: $idade"
+
+data=$(date +"%d-%m-%y_%T")
+echo "Execução realizada em $data"
+
+echo 'Até a próxima.'
